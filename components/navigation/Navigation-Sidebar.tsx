dc2fd86d52669322b83b2b14d2c2f98e7ecc7930
WYZ00 <thanthtettin@gmail.com>
2023-09-01 22:37:41 +0630

    create server modal

--- components/navigation/Navigation-Sidebar.tsx
+++ components/navigation/Navigation-Sidebar.tsx
@@ -1,11 +1,13 @@
 import { currentProfile } from "@/lib/current-profile";
 import { redirect } from "next/navigation";
+import { UserButton } from "@clerk/nextjs";
 import { db } from "@/lib/db";
 
 import { NavigationAction } from "./Navigation-Action";
 import { Separator } from "@/components/ui/separator";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { NavigationItem } from "./NavigationItem";
+import { ModeToggle } from "@/components/mode-toggle";
 
 export const NavigationSidebar = async () => {
   const profile = await currentProfile();
@@ -39,6 +41,17 @@ export const NavigationSidebar = async () => {
           </div>
         ))}
       </ScrollArea>
+      <div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
+        <ModeToggle />
+        <UserButton
+          afterSignOutUrl="/"
+          appearance={{
+            elements: {
+              avatarBox: "h-[48px] w-[48px]",
+            },
+          }}
+        />
+      </div>
     </div>
   );
 };
